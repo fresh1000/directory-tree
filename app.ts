5a25eccf1f967949ce0cbeff3959efd6fcf0dd97
Yurii Ivashchenko <fresh19101995@gmail.com>
2020-07-25 15:59:20 +0300

    fix configs

--- app.ts
+++ app.ts
@@ -1,13 +1,5 @@
-import * as dotenv from 'dotenv'
 import app from './src/index'
 import { sequelize } from './src/configSequelize'
-
-if (process.env.DOTENV) {
-  process.env.DOTENV
-    .split(',')
-    .filter((val) => val)
-    .forEach((envPath) => dotenv.config({ path: envPath }))
-}
 
 const port = process.env.PORT || 4000
 
--- src/configSequelize.ts
+++ src/configSequelize.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize'
+require('dotenv').config()
 
-export const sequelize = new Sequelize('directory_tree', 'root', '', {
-  host: 'localhost',
+export const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, {
+  host: process.env.DATABASE_HOST,
   dialect: 'mysql',
 })
